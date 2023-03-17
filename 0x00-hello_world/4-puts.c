1e582b883f2ff99022306942a8da9ff70006c5f0
oussama <marhraoui.oussama@gmail.com>
2023-03-16 18:11:35 -0700

    puts function task

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,7 @@
 #include <stdio.h>
-/* A program that use puts function to print */
-
 int main(void)
+/* A program that use puts function to print */
 {
-  puts("\"Programming is like building a multilingual puzzle");
-  return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
