fc07aa71bf1ce6819b6c29706398c4843e7c2f3b
oussama <marhraoui.oussama@gmail.com>
2023-03-17 16:55:09 -0700

    second attempt

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,9 +1,11 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-/* more headers goes there */
-
-/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ *Description - check if a n is negatif or not
+ *Return - always 0
+*/
 int main(void)
 {
   int n;
