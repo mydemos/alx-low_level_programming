535291ebcc82cfd33bd75eb0248c092ec4998f61
oussama <marhraoui.oussama@gmail.com>
2023-03-21 17:45:27 -0700

    first task

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,6 +1,22 @@
-#include <stdio.h>
+#include "main.h"
 
-int main(){
-  printf("_putchar");
-  return (0);
+/**
+ * main - Prints _Putchar as a message.
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char text[10] = "_putchar";
+	int i = 0;
+
+	for (i = 0; i < 9; i++)
+	{
+		_putchar(text[i]);
+	}
+	_putchar('\n');
+
+	return (0);
 }
+
