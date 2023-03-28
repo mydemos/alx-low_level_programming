1e9dce9ba0805b7eb0e509c8f112af915a7fc463
oussama <marhraoui.oussama@gmail.com>
2023-03-27 17:46:13 -0700

    task 0-5 solution

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * rev_string - reverse string
+ * @s: string
+ * Return: void
+ */
+
+void rev_string(char *s)
+{
+int i;
+int counter =0;
+	char rev = s[0];
+
+	while (s[counter] != '\0')
+	{
+		counter++;
+	}
+       
+	for (i = 0; i < counter; i++)
+	{
+		counter--;
+	      	rev = s[i];
+		s[i] = s[counter];
+		s[counter] = rev;
+	}
+}
